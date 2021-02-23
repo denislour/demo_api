f0f597014401792cad862f696f32ec3af87c7428
Lektor <denislour@gmail.com>
2021-02-23 12:17:21 +0700

    Convert function base views to class base views

--- core/views.py
+++ core/views.py
@@ -1,10 +1,21 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-def test_view(request):
-    data = {
-        'name': 'Son',
-        'age': '28',
-    }
-    return JsonResponse(data)
+class TestView(APIView):
+
+    def get(self, request):
+        data = {
+            'name': 'Son',
+            'age': 28,
+        }
+        return Response(data)
+
+# def test_view(request):
+#     data = {
+#         'name': 'Son',
+#         'age': '28',
+#     }
+#     return JsonResponse(data)
